812c38f8aaae9f3f705661fb1a6127dd2c6a6b29
Ethan Wall <ewall25@jesuittampa.org>
2025-03-13 01:18:27 -0400

    Enable logging

--- src/robot.py
+++ src/robot.py
@@ -1,12 +1,16 @@
 from typing import Optional
 
 import commands2
+from wpilib import DataLogManager
 
 from container import RobotContainer
 
 
 class Robot(commands2.TimedCommandRobot):
     def robotInit(self):
+        # Start recording NetworkTables to data log
+        DataLogManager.start()
+
         self.container = RobotContainer()
         self.scheduler = commands2.CommandScheduler.getInstance()
         self.autonomous_command: Optional[commands2.Command] = None
